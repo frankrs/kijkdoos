b0f21ce629c73c0ce4a23d1c5cd7075cfde92fca
frankrs <frankstapleton3d@gmail.com>
2015-05-07 05:28:01 -0400

    escape to fly

---
+++ Assets/Franks Shit/Scripts/BalloonEngine.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonEngine : MonoBehaviour {
+
+
+	public float gas = 12f;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(StaticShit.gasOn){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,gas));
+		}
+	}
+}
---
+++ Assets/Franks Shit/Scripts/Lift.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lift : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		StaticShit.gasOn = Input.GetKey("escape");
+	}
+}
