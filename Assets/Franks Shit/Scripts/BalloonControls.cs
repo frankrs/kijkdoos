e7c49ca3541e1e81577d35c7b84020ae25de9f18
frankrs <frankstapleton3d@gmail.com>
2015-05-07 03:16:39 -0400

    simple up with the spacebar

---
+++ Assets/Franks Shit/Scripts/BalloonControls.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonControls : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	//sort out the input
+		StaticShit.gasOn = Input.GetButton("Gas");
+	}
+}
+
+
+[System.Serializable]
+public static class StaticShit{
+
+	public static bool gasOn;
+
+}
---
+++ Assets/Franks Shit/Scripts/BalloonMech.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BalloonMech : MonoBehaviour {
+
+	public float gas;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(StaticShit.gasOn){
+			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,gas));
+		}
+	}
+}
